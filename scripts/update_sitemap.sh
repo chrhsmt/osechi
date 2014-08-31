a54998be1a30489f9b1189e7ac20d4ce6886127a
chrhsmt <chr@chrhsmt.com>
2014-09-01 01:08:15 +0900

    change update_sitemap script

--- scripts/update_sitemap.sh
+++ scripts/update_sitemap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl http://www.google.com/webmasters/tools/ping?sitemap=http://xn--t8j8as.co/rss.xml
+curl http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fxn--t8j8as.co%2Frss.xml
