68d6efb83fecbab8188d8a1461e10a6d5e2aed0b
chrhsmt <chr@chrhsmt.com>
2014-09-01 19:36:38 +0900

    add host to ping

--- scripts/update_sitemap.sh
+++ scripts/update_sitemap.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-curl http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2Fxn--t8j8as.co%2Frss.xml
+SITE=http%3A%2F%2Fxn--t8j8as.co%2Frss.xml
+
+curl http://www.google.com/webmasters/tools/ping?sitemap=$SITE
+curl -XGET http://google.jp/ping?sitemap=$SITE
